1e10dcd90e879a0510fbd9767dfdaca098b82f4d
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-05-28 15:01:43 -0500

    create dictionaries to map between books and their data quickly

--- src/library.py
+++ src/library.py
@@ -1,42 +1,80 @@
 import os
 import json
 
+from collections import defaultdict
+
 from book import Book
 
 class Library:
     def __init__(self) -> None:
-        self.books: list[Book] = []
+        self.books:             list[Book]             = []
+        self.index_from_title:  defaultdict[str, list] = defaultdict(list)
+        self.index_from_author: defaultdict[str, list] = defaultdict(list)
+        self.index_from_isbn:   dict[str, int]         = {}
 
     def add_book(self, book: Book) -> None:
         if self.book_by_isbn(book.isbn):
-            print('Book already exists.')
+            print(f'{book.title}, ISBN: {book.isbn} already exists.')
         else:
             self.books.append(book)
-            print('Book added successfully.')
+
+            book_index = len(self.books) - 1
+
+            self.index_from_title[book.title].append(book_index)
+            self.index_from_author[book.author].append(book_index)
+            self.index_from_isbn[book.isbn] = book_index
+
+            print(f'{book.title}, ISBN: {book.isbn} added successfully.')
 
     def remove_book(self, isbn: str) -> None:
         book = self.book_by_isbn(isbn)
 
         if book:
-            self.books.remove(book)
-            print('Book removed successfully.')
-        else:
-            print('Book not found.')
+            book_index = self.index_from_isbn[isbn]
 
-    def list_books(self) -> list[Book]:
-        return self.books
+            del self.books[book_index]
+
+            # we want to remove the book_index itself from the list, not the value which book_index
+            # points to
+            self.index_from_title[book.title].remove(book_index)
+            if not self.index_from_title[book.title]:
+                del self.index_from_title[book.title]
+
+            self.index_from_author[book.author].remove(book_index)
+            if not self.index_from_author[book.author]:
+                del self.index_from_author[book.author]
+
+            del self.index_from_isbn[isbn]
+
+            for title in self.index_from_title:
+                self.index_from_title[title] = [i - 1 if i > book_index else i for i in
+                                                self.index_from_title[title]]
+
+            for author in self.index_from_author:
+                self.index_from_author[author] = [i - 1 if i > book_index else i for i in
+                                                  self.index_from_author[author]]
+
+            for isbn_key, idx in self.index_from_isbn.items():
+                if idx > book_index:
+                    self.index_from_isbn[isbn_key] -= 1
+
+            print(f'{book.title}, ISBN: {isbn} removed successfully.')
+        else:
+            print(f'ISBN: {isbn} not found.')
+
+    def books_by_title(self, title: str) -> list[Book]:
+        return [self.books[i] for i in self.index_from_title[title]]
+
+    def books_by_author(self, author: str) -> list[Book]:
+        return [self.books[i] for i in self.index_from_author[author]]
 
     def book_by_isbn(self, isbn: str) -> Book | None:
-        for book in self.books:
-            if book.isbn == isbn:
-                return book
+        try:
+            return self.books[self.index_from_isbn[isbn]]
+        except KeyError:
+            return None
 
-        return None
-
-    def books_by_author(self, author):
-        return [book for book in self.books if book.author == author]
-
-    def update_file(self, file_path: str):
+    def update_file(self, file_path: str) -> None:
         existing_file = os.path.isfile(file_path)
 
         books_data = [vars(book) for book in self.books]
@@ -72,7 +110,7 @@ class Library:
                 else:
                     print('No books in list. File not created.')
 
-    def load_file(self, file_path: str):
+    def load_file(self, file_path: str) -> None:
         try:
             with open(file_path, 'r', encoding='utf-8') as json_file:
                 books_data = json.load(json_file)
